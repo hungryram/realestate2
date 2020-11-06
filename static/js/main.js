bb7999dc5119d9371522afdedab69aff3dc6bc1f
Ram Dettmer <ramdettmer751@gmail.com>
2020-11-05 16:23:57 -0800

    updates

--- static/js/main.js
+++ static/js/main.js
@@ -33,3 +33,55 @@ $('.responsive-slide').slick({
       // instead of a settings object
     ]
   });
+
+// HERO
+
+jQuery(document).ready(function ($) {
+  $('.home-slide').slick({
+  dots: false,
+      pauseOnHover: false,
+      infinite: true,
+      speed: 500,
+      fade: !0,
+      cssEase: 'linear',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+      autoplay: true,
+  autoplaySpeed: 5000,
+      draggable: false,
+  arrows: false,
+  responsive: [
+    {
+  breakpoint: 1024,
+  settings: {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+      infinite: true
+            }
+      },
+      {
+  breakpoint: 768,
+  settings: {
+      draggable: true,
+            }
+  },
+  {
+  breakpoint: 600,
+  settings: {
+      slidesToShow: 1,
+      draggable: true,
+  slidesToScroll: 1
+          }
+  },
+  {
+  breakpoint: 480,
+  settings: {
+      slidesToShow: 1,
+      draggable: true,
+  slidesToScroll: 1
+            }
+  }
+
+          ]
+                });
+      });
